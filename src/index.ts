1655e3547eca2983bbb585810bbd5cfe87f73abb
Magizchi <g.rajan1993@gmail.com>
2023-02-22 22:44:07 +0100

    its alive

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-const EJAN = "EJAN";
-console.log('e', EJAN);
+import bodyParser from "body-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+    try {
+        res.send("Nodejs Started");
+    } catch (err) {
+        throw `Message: ${err}`;
+    }
+});
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log('Server started');
+});
