058ea288b7f28e38eacbc85ff39ce819e1e3ec84
Magizchi <g.rajan1993@gmail.com>
2023-02-24 16:14:28 +0000

    api:feat<migrations>

---
+++ src/migrations/index.ts
@@ -0,0 +1,4 @@
+import { astronaute1677228911190 } from "./1677228911190-astronaute.js";
+import { planets1677228964151 } from "./1677228964151-planets.js";
+
+export const Migrations = [astronaute1677228911190, planets1677228964151]
