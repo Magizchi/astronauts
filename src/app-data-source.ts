3e7b449ea6d0dd61a0968e3e00614cedc5934b1f
Magizchi <g.rajan1993@gmail.com>
2023-02-24 16:16:10 +0000

    api:feat<app-data-source>typeorm connect to bdd

---
+++ src/app-data-source.ts
@@ -0,0 +1,32 @@
+import { DataSource } from "typeorm";
+import * as  mysql from "mysql"
+import dotenv from "dotenv";
+import { Entities } from './entities/index.js';
+import { Migrations } from './migrations/index.js';
+
+dotenv.config();
+
+const ENV = {
+    host: process.env.DB_HOST ?? "db",
+    port: Number(process.env.DB_PORT) ?? 3306,
+    userName: process.env.DB_USER ?? "",
+    password: process.env.DB_PASSWORD ?? "",
+    dataBase: process.env.DB_DATABASE ?? "astronaute",
+    synchronize: process.env.DB_SYNCHRO === "true" ? true : false
+}
+
+const dataSource = new DataSource({
+    driver: mysql,
+    type: "mariadb",
+    host: ENV.host,
+    port: ENV.port,
+    username: ENV.userName,
+    password: ENV.password,
+    database: ENV.dataBase,
+    entities: [...Entities],
+    migrations: [...Migrations],
+    logging: false,
+    synchronize: ENV.synchronize,
+})
+
+export default dataSource
