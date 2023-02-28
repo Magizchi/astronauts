9eb56e197875708875afee0f5bd990943d5f1479
Magizchi <g.rajan1993@gmail.com>
2023-02-28 09:26:16 +0000

    client:refacto<constant:API>

---
+++ client/src/constants/API.ts
@@ -0,0 +1,6 @@
+export enum API {
+    PLANETS = "/api/planets",
+    ASTRONAUTES = "/api/astronautes",
+    CREATE_ASTRONAUTES = "/api/astronautes/create",
+    UPDATE_ASTRONAUTES = "/api/astronautes/update/"
+}
