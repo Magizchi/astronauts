1215e8e8dcf33ffb82ccc41b66f56c51636705e7
Magizchi <g.rajan1993@gmail.com>
2023-03-01 22:34:29 +0000

    api:feat<migration> added

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,3 +1,5 @@
 import { planetsSeeder1677347889754 } from "./1677347889754-planets-seeder.js"
 import { astronautes1677496029304 } from "./1677496029304-astronautes.js"
-export const Migrations = [planetsSeeder1677347889754, astronautes1677496029304]
+import { renameAstronauts1677708786442 } from "./1677708786442-rename_astronauts.js"
+import { changePlanets1677709310156 } from "./1677709310156-change_planets.js"
+export const Migrations = [planetsSeeder1677347889754, astronautes1677496029304, renameAstronauts1677708786442, changePlanets1677709310156]
