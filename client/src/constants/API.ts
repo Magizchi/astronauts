31b0a0138ed0299ff6bce44e942cd06974ecc5ef
Magizchi <g.rajan1993@gmail.com>
2023-03-01 22:08:38 +0000

    clien:refacto<contant:API>delete ussless keys

--- client/src/constants/API.ts
+++ client/src/constants/API.ts
@@ -1,6 +1,4 @@
 export enum API {
     PLANETS = "/api/planets",
     ASTRONAUTES = "/api/astronautes",
-    CREATE_ASTRONAUTES = "/api/astronautes/create",
-    UPDATE_ASTRONAUTES = "/api/astronautes/update/"
 }
