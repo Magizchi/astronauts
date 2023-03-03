3169996925e1e95f53f6b10d4977522593053ce3
Magizchi <g.rajan1993@gmail.com>
2023-03-03 20:59:21 +0000

    client:feat<atom:Link>creat component Link

---
+++ client/src/components/atoms/Link.tsx
@@ -0,0 +1,27 @@
+import classNames from 'classnames';
+import React, { FunctionComponent } from 'react'
+
+interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+    variant: "solid" | "outline";
+    color?: "primary" | "secondary";
+    to: string;
+}
+
+export const Link: FunctionComponent<LinkProps> = ({ variant, to, children }) => {
+    return (
+        <a
+            href={to}
+            className={classNames(
+                "block w-full rounded px-12 py-3 text-sm font-medium shadow focus:outline-none focus:ring sm:w-auto",
+                {
+                    "bg-rose-600 hover:bg-rose-700 active:bg-rose-500 text-white ": variant === 'solid',
+                    "bg-white text-rose-600 hover:text-rose-700 active:text-rose-500 ": variant === "outline"
+                }
+            )}
+        >
+            {children}
+        </a>
+
+
+    )
+}
