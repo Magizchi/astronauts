ead66848a748195b9bd1f0fff3c88ae107e03ed4
Magizchi <g.rajan1993@gmail.com>
2023-03-05 15:35:37 +0000

    api:feat<route:companies>get companies info

---
+++ src/routes/companies.ts
@@ -0,0 +1,27 @@
+import { Router, Request, Response } from "express";
+import myDataSource from "../app-data-source.js";
+import { Company } from "../entities/company.entity.js";
+
+const router = Router();
+const CompaniesRepository = myDataSource.getRepository(Company);
+
+router.get('/:url', async (req: Request, res: Response) => {
+    try {
+        const getCompanyAndArticle = await CompaniesRepository.findOne({
+            where: { url: req.params.url },
+            relations: {
+                article: {
+                    spaceShips: true
+                },
+            }
+        });
+
+        res.status(200).json(getCompanyAndArticle);
+    } catch (err) {
+        res.status(500);
+    }
+});
+
+
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import Planets from './planets.js';
 import Astronauts from './astronauts.js';
+import Companies from './companies.js';
 
 const router = Router();
 
 router.use("/planets", Planets);
 router.use("/astronauts", Astronauts);
+router.use("/companies", Companies);
 
 export default router;
