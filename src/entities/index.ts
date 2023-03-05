1ae49924ba762b36d2dce5fcfa8714762b900a6f
Magizchi <g.rajan1993@gmail.com>
2023-03-05 15:29:26 +0000

    api:feat<entities>imported

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,4 +1,7 @@
 import { Planet } from "./planet.entity.js";
 import { Astronaut } from "./astronaut.entity.js";
+import { spaceShip } from "./spaceShip.entity.js";
+import { Article } from "./article.entity.js";
+import { Company } from "./company.entity.js";
 
-export const Entities = [Planet, Astronaut];
+export const Entities = [Planet, Astronaut, spaceShip, Article, Company];
