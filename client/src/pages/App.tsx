72007a9497b778a18665afd310229de86a11edf0
Magizchi <g.rajan1993@gmail.com>
2023-03-05 15:20:33 +0000

    client:feat<app:Router>create router

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,19 +1,21 @@
+import axios from 'axios';
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { PATH } from '../constants/Routes'
+import Layout from '../layouts/Layout';
 import Companies from './companies';
 import DoIt from './doIt';
 import Home from './home'
 
 const App = () => (
-    <main>
-        <BrowserRouter>
-            <Routes>
-                <Route path={PATH.HOME} element={<Home />} />
-                <Route path={PATH.DOIT} element={<DoIt />} />
-                <Route path={PATH.COMPANIES} element={<Companies />} />
-            </Routes>
-        </BrowserRouter>
+    <main className="bg-black">
+        <Routes>
+            <Route path={PATH.HOME} element={<Home />} />
+            <Route path={PATH.DOIT} element={<DoIt />} />
+            <Route path={PATH.COMPANIES} element={<Layout />}>
+                <Route path=':name' element={<Companies />} />
+            </Route>
+        </Routes>
     </main>
 );
 
