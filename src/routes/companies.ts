be1214ac89af455ae76e42d7a5ab056e75379bad
Magizchi <g.rajan1993@gmail.com>
2023-03-05 17:06:18 +0000

    api:feat<route:companie>get list of companie

--- src/routes/companies.ts
+++ src/routes/companies.ts
@@ -5,6 +5,15 @@ import { Company } from "../entities/company.entity.js";
 const router = Router();
 const CompaniesRepository = myDataSource.getRepository(Company);
 
+router.get('/navigation', async (_req: Request, res: Response) => {
+    try {
+        const getCompaniesForNavigation = await CompaniesRepository.find();
+        res.status(200).json(getCompaniesForNavigation);
+    } catch (err) {
+        res.status(500);
+    }
+});
+
 router.get('/:url', async (req: Request, res: Response) => {
     try {
         const getCompanyAndArticle = await CompaniesRepository.findOne({
