a7f410a6ddee4fe23778894f12ffb6e94aa5b876
Magizchi <g.rajan1993@gmail.com>
2023-03-06 13:28:43 +0000

    client:style<page:doIt>add margin bottom

--- client/src/pages/doIt/index.tsx
+++ client/src/pages/doIt/index.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import { PATH } from '../../constants/Routes';
+import { PATH } from "../../constants/Routes";
 
 const DoIt = () => {
     return (
         <section className="flex flex-col justify-center items-center h-screen bg-black">
             <img src={"https://cdn.dribbble.com/userupload/3592203/file/original-170c48e0976d8e7582a6e7f411a685ec.png?compress=1&resize=1024x768"} className="bg-clip-content p-5" />
-            <div className='flex items-center'>
+            <div className="flex items-center mb-20">
                 <h2 className="text-5xl text-white font-bold">Do It!</h2>
                 <Link
                     className="ml-5 inline-block rounded-full border border-indigo-600 bg-indigo-600 p-3 text-white hover:bg-white hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
