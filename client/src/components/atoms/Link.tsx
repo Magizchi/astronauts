6b457571c397b8a7526e9a4854f8ba9771fb2d4d
Magizchi <g.rajan1993@gmail.com>
2023-03-06 13:29:11 +0000

    client:style<atom:Link>add padding

--- client/src/components/atoms/Link.tsx
+++ client/src/components/atoms/Link.tsx
@@ -13,7 +13,7 @@ export const Link: FunctionComponent<LinkProps> = ({ variant, to, children }) =>
         <LinkRouter
             to={to}
             className={classNames(
-                "block w-full rounded px-12 py-3 text-sm font-medium shadow focus:outline-none focus:ring sm:w-auto",
+                "block w-full rounded px-5 py-3 mx-2 text-sm font-medium shadow text-center focus:outline-none focus:ring sm:w-auto",
                 {
                     "bg-rose-600 hover:bg-rose-700 active:bg-rose-500 text-white ": variant === 'solid',
                     "bg-white text-rose-600 hover:text-rose-700 active:text-rose-500 ": variant === "outline"
