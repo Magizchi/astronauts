bc87444dedd26aab7809ab03826ecd39067c3be4
Magizchi <g.rajan1993@gmail.com>
2023-03-07 15:32:54 +0000

    client:style<layout>fix prettier

--- client/src/layouts/Header.tsx
+++ client/src/layouts/Header.tsx
@@ -1,23 +1,23 @@
-import { FunctionComponent } from 'react';
-import { Link } from 'react-router-dom';
-import Button from '../components/atoms/button';
-import Navigation from '../components/organisms/Navigation';
-import { PATH } from '../constants/Routes';
-import { Navigation as NavigationType } from '../models/navigation';
+import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
+import Button from "../components/atoms/button";
+import Navigation from "../components/organisms/Navigation";
+import { PATH } from "../constants/Routes";
+import { Navigation as NavigationType } from "../models/navigation";
 
 interface HeaderProps {
-    navigation: NavigationType[]
+  navigation: NavigationType[];
 }
 const Header: FunctionComponent<HeaderProps> = ({ navigation }) => {
-    return (
-        <header className="flex justify-between items-center py-5 px-8">
-            <Link to={PATH.HOME}>
-                <h1 className="text-white text-4xl">Astronauts</h1>
-            </Link>
-            <Navigation navigation={navigation} />
-            <Button />
-        </header>
-    )
-}
+  return (
+    <header className="relative flex justify-center sm:justify-around lg:justify-between xl:items-center xl:py-5 xl:px-8">
+      <Link to={PATH.HOME} className="hidden py-2 sm:block">
+        <h1 className="text-4xl text-white">Astronauts</h1>
+      </Link>
+      <Navigation navigation={navigation} />
+      <Button className="hidden sm:block" />
+    </header>
+  );
+};
 
-export default Header
+export default Header;
